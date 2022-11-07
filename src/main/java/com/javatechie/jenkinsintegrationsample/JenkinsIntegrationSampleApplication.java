a832e3939b9b7f77f669ba119194360bb72eb6e1
hennible0612 <sungwoo166@gmail.com>
2022-11-07 23:09:17 +0900

    TEST : Jenkins test

--- src/main/java/com/javatechie/jenkinsintegrationsample/JenkinsIntegrationSampleApplication.java
+++ src/main/java/com/javatechie/jenkinsintegrationsample/JenkinsIntegrationSampleApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JenkinsIntegrationSampleApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello Jenkins");
 		SpringApplication.run(JenkinsIntegrationSampleApplication.class, args);
 	}
 
