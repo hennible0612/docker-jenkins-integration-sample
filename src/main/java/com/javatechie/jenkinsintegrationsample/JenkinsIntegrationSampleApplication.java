97934aea80066715a2369fd011806e5f5cc7b0aa
hennible0612 <sungwoo166@gmail.com>
2022-11-07 20:22:05 +0900

    init

---
+++ src/main/java/com/javatechie/jenkinsintegrationsample/JenkinsIntegrationSampleApplication.java
@@ -0,0 +1,13 @@
+package com.javatechie.jenkinsintegrationsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsIntegrationSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsIntegrationSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javatechie/jenkinsintegrationsample/JenkinsIntegrationSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.jenkinsintegrationsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsIntegrationSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
