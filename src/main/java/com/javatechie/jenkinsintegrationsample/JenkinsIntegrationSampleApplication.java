dba0a86d6a6b1a3c6afc175f492cac8f30f47d3c
hennible0612 <sungwoo166@gmail.com>
2022-11-12 00:05:28 +0900

    .

--- src/main/java/com/javatechie/jenkinsintegrationsample/JenkinsIntegrationSampleApplication.java
+++ src/main/java/com/javatechie/jenkinsintegrationsample/JenkinsIntegrationSampleApplication.java
@@ -2,10 +2,18 @@ package com.javatechie.jenkinsintegrationsample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
+@RestController
 public class JenkinsIntegrationSampleApplication {
 
+	@GetMapping("/message")
+	public String getMessage(){
+		return "Welcome to main?";
+	}
 	public static void main(String[] args) {
 		System.out.println("Hello Jenkins");
 		SpringApplication.run(JenkinsIntegrationSampleApplication.class, args);
